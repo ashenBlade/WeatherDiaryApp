7b77cb346567b291e72b1a5b81ffb489fd87269e
Danil Bogomolov <danil.bogo@gmail.com>
2021-06-27 14:35:21 +0300

    weather record

---
+++ WeatherDiaryApp/Server/Models/WeatherRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Server.Models
+{
+    public class WeatherRecord
+    {
+        private string city { get; set; }
+        private DateTime date { get; set; }
+        private TimesOfDay timeOfDay { get; set; }
+        private WeatherIndicator weatherIndicator { get; set; }
+
+        public WeatherRecord GetWeatherRecord()
+        {
+            return this;
+        }
+
+        public void SetWeatherRecord(string city, 
+            DateTime date, 
+            TimesOfDay timeOfDay, 
+            WeatherIndicator weatherIndicator)
+        {
+            this.city = city;
+            this.date = date;
+            this.timeOfDay = timeOfDay;
+            this.weatherIndicator = weatherIndicator;
+        }
+    }
+}
