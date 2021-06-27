096d7ded86f2576aa876e25e2c5776a1e2a4de8a
Danil Bogomolov <danil.bogo@gmail.com>
2021-06-27 15:10:30 +0300

    little changes

--- WeatherDiaryApp/Server/Models/WeatherRecord.cs
+++ WeatherDiaryApp/Server/Models/WeatherRecord.cs
@@ -4,25 +4,9 @@ namespace Server.Models
 {
     public class WeatherRecord
     {
-        private string city { get; set; }
-        private DateTime date { get; set; }
-        private TimesOfDay timeOfDay { get; set; }
-        private WeatherIndicator weatherIndicator { get; set; }
-
-        public WeatherRecord GetWeatherRecord()
-        {
-            return this;
-        }
-
-        public void SetWeatherRecord(string city, 
-            DateTime date, 
-            TimesOfDay timeOfDay, 
-            WeatherIndicator weatherIndicator)
-        {
-            this.city = city;
-            this.date = date;
-            this.timeOfDay = timeOfDay;
-            this.weatherIndicator = weatherIndicator;
-        }
+        public string City { get; set; }
+        public DateTime Date { get; set; }
+        public TimesOfDay TimeOfDay { get; set; }
+        public WeatherIndicator WeatherIndicator { get; set; }
     }
 }
