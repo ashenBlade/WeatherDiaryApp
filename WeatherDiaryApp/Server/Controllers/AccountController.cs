8cdf5adc6341504d1c88b87c8b101d3c3eb52b7d
ashblade <htcdnsnnkom2002@gmail.com>
2021-06-27 14:44:00 +0300

    Add logout functionality

--- WeatherDiaryApp/Server/Controllers/AccountController.cs
+++ WeatherDiaryApp/Server/Controllers/AccountController.cs
@@ -1,3 +1,6 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Server.Models;
 
@@ -45,5 +48,12 @@ namespace Server.Controllers
             // Error - otherwise
             return RedirectToAction("Login");
         }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+
     }
 }
