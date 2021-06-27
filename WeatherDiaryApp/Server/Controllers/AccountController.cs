d8d1398983466562e64332833998554f030631ec
ashblade <htcdnsnnkom2002@gmail.com>
2021-06-27 14:53:43 +0300

    Add helper method for authentication

--- WeatherDiaryApp/Server/Controllers/AccountController.cs
+++ WeatherDiaryApp/Server/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Server.Models;
 
@@ -55,5 +58,15 @@ namespace Server.Controllers
             return RedirectToAction("Login", "Account");
         }
 
+        private static async Task Authenticate(HttpContext context, User user)
+        {
+            var claims = new List<Claim>() { new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email) };
+            var id = new ClaimsIdentity(claims, "ApplicationCookie",
+                                        ClaimsIdentity.DefaultNameClaimType,
+                                        ClaimsIdentity.DefaultRoleClaimType);
+
+            var principal = new ClaimsPrincipal(id);
+            await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+        }
     }
 }
