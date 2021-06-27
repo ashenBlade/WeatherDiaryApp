9d224f105603dd8cd79224f414f649673913d9a2
ashblade <htcdnsnnkom2002@gmail.com>
2021-06-27 14:03:03 +0300

    Add basic login register empty actions with views

--- WeatherDiaryApp/Server/Controllers/AccountController.cs
+++ WeatherDiaryApp/Server/Controllers/AccountController.cs
@@ -1,13 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
+using Server.Models;
 
 namespace Server.Controllers
 {
     public class AccountController : Controller
     {
-        // GET
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            return Ok();
         }
     }
 }
