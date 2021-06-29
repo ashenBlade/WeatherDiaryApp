34ea511cd394f2b659ffa3e161add0e54db55a15
ashblade <htcdnsnnkom2002@gmail.com>
2021-06-29 11:55:58 +0300

    Add suffix with ViewModel to models

--- WeatherDiaryApp/Server/Models/LoginViewModel.cs
+++ WeatherDiaryApp/Server/Models/LoginViewModel.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Server.Models
 {
-    public class LoginModel
+    public class LoginViewModel
     {
         [Required]
         public string Email { get; set; }
