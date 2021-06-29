92f03ff0d4994872513cf56f4a9d3c8e5f8351dc
ashblade <htcdnsnnkom2002@gmail.com>
2021-06-29 07:28:58 +0300

    Fix first-merge error

--- WeatherDiaryApp/Server/Program.cs
+++ WeatherDiaryApp/Server/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using WeatherDiary.Data;
 using Server.Models;
 using System;
 using Server;
