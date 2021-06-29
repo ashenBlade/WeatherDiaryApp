c987d62bd585fa120494e0e923e73a0db45a7ae3
ashblade <htcdnsnnkom2002@gmail.com>
2021-06-29 12:33:27 +0300

    Add DisplayAttributes for russian language to display

--- WeatherDiaryApp/Server/Models/LoginViewModel.cs
+++ WeatherDiaryApp/Server/Models/LoginViewModel.cs
@@ -5,10 +5,12 @@ namespace Server.Models
     public class LoginViewModel
     {
         [Required]
+        [Display(Name = "Почта")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
     }
 }
