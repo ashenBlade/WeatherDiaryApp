daa0681f73cbc62fbd5ef1747f43953ee06a69ec
ashblade <htcdnsnnkom2002@gmail.com>
2021-06-30 09:39:40 +0300

    Add basic params and methods
    Add base url and api key strings,
    constructor and method for getting request url by city name

--- WeatherDiaryApp/Api/WeatherApi/WeatherApiApiRequester.cs
+++ WeatherDiaryApp/Api/WeatherApi/WeatherApiApiRequester.cs
@@ -1,3 +1,5 @@
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Common;
 
@@ -5,6 +7,18 @@ namespace Api.WeatherApi
 {
     public class WeatherApiApiRequester : IWeatherApiRequester
     {
+        private const string _baseUrl = "http://api.weatherapi.com/v1/current.json";
+        private const string _apiKey = "9429cccf088748ff84b55742212706";
+        private HttpClient _client;
+
+        public WeatherApiApiRequester()
+        {
+            _client = new HttpClient();
+        }
+
+        private static string GetRequestUrl(string city) =>
+            $"{_baseUrl}?q={city}&key={_apiKey}";
+
         public WeatherIndicator GetRecord(string city)
         {
             throw new System.NotImplementedException();
