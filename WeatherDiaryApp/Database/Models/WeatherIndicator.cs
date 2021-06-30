4fb05ad4ab350a4cc9fcbab20693aa32d2bb9975
Danil Bogomolov <danil.bogo@gmail.com>
2021-06-30 12:26:00 +0300

    add int? to fields Pressure, Temperature and WindSpeed

--- WeatherDiaryApp/Database/Models/WeatherIndicator.cs
+++ WeatherDiaryApp/Database/Models/WeatherIndicator.cs
@@ -8,9 +8,9 @@ namespace Database
         public int Id { get; set; }
         public Phenomena Phenomena { get; set; }
         public Precipitation Precipitation { get; set; }
-        public int Pressure { get; set; }
-        public int Temperature { get; set; }
+        public int? Pressure { get; set; }
+        public int? Temperature { get; set; }
         public WindDirection WindDirection { get; set; }
-        public int WindSpeed { get; set; }
+        public int? WindSpeed { get; set; }
     }
 }
