3b3f7c89618e53a0549a60f9380e48701311dde6
LipatovAlexander <lipatov.work@bk.ru>
2021-06-30 15:42:39 +0300

    add seed for weather records

--- WeatherDiaryApp/Database/SeedData/Initializer.cs
+++ WeatherDiaryApp/Database/SeedData/Initializer.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using CsvHelper;
 using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using CsvHelper.TypeConversion;
 
 namespace Database.SeedData
 {
@@ -15,18 +17,31 @@ namespace Database.SeedData
     {
         public void SeedCity (WeatherDiaryContext context)
         {
-            using (StreamReader reader = new StreamReader(@"../Database/SeedData/city.csv"))
+            using StreamReader reader = new StreamReader(@"../Database/SeedData/city.csv");
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-                {
-                    MissingFieldFound = null,
-                    HasHeaderRecord = false,
-                };
-                CsvReader csvReader = new CsvReader(reader, config);
-                var cities = csvReader.GetRecords<City>().ToArray();
-                context.Cities.AddRange(cities);
-                context.SaveChanges();
-            }
+                MissingFieldFound = null,
+                HasHeaderRecord = false,
+            };
+            CsvReader csvReader = new CsvReader(reader, config);
+            var cities = csvReader.GetRecords<City>().ToArray();
+            context.Cities.AddRange(cities);
+            context.SaveChanges();
+        }
+
+        public void SeedWeather (WeatherDiaryContext context)
+        {
+            using StreamReader reader = new StreamReader(@"../Database/SeedData/records.csv");
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                MissingFieldFound = null,
+                HeaderValidated = null,
+            };
+            CsvReader csvReader = new CsvReader(reader, config);
+            var records = csvReader.GetRecords<WeatherRecord>().ToArray();
+            Array.ForEach(records, r => r.City = null);
+            context.WeatherRecords.AddRange(records);
+            context.SaveChanges();
         }
     }
 }
