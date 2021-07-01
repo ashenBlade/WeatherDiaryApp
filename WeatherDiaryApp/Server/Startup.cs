ed281fc6607863008d84515b4a48e15f4651e093
ashblade <htcdnsnnkom2002@gmail.com>
2021-07-01 06:56:17 +0300

    Add if statement for removing api calls while debugging

--- WeatherDiaryApp/Server/Startup.cs
+++ WeatherDiaryApp/Server/Startup.cs
@@ -42,7 +42,9 @@ namespace Server
                               );
             ConfigureRepository(services, Configuration);
             services.AddScoped<IWeatherApiRequester, WeatherApiApiRequester>();
+#if !DEBUG
             services.AddHostedService<WeatherUpdaterHostedService>();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
