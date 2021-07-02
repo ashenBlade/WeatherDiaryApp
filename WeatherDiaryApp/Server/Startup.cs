fc6651a271e00e67361bac12f3572a18efdca3f7
ashblade <htcdnsnnkom2002@gmail.com>
2021-07-02 07:06:51 +0300

    Remove preprocessor 'if' in adding hosted service for api requests

--- WeatherDiaryApp/Server/Startup.cs
+++ WeatherDiaryApp/Server/Startup.cs
@@ -42,9 +42,7 @@ namespace Server
                               );
             ConfigureRepository(services, Configuration);
             services.AddScoped<IWeatherApiRequester, WeatherApiApiRequester>();
-#if !DEBUG
             services.AddHostedService<WeatherUpdaterHostedService>();
-#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
